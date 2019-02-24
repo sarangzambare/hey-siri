2561bc0343a4bed0daa863cb37903263ac2cd126
Sarang Zambare <thesarng@gmail.com>
2019-02-24 14:17:53 -0800

    fixed output shape

--- model.py
+++ model.py
@@ -7,24 +7,25 @@ from keras.layers import GRU, Bidirectional, BatchNormalization, Reshape
 from keras.optimizers import Adam
 from audio_data import graph_spectrogram
 import numpy as np
-x = graph_spectrogram("train.wav")
 
-x.shape
+
 
 Y = np.load('train_dir/Y.npy')
 Y_test = np.load('train_dir/Y_test.npy')
 
+# number of frequncies in fourier decomposition
+freq_n = 101
+# number of samples in the audio clip
+sample_n = 1998
+
 train_dir = "train_dir"
 
 Y = Y.reshape(1000,1375,4)
-
-Y = Y[:200,:,:]
-Y.shape
 Y_test = Y_test.reshape(100,1375,4)
 
 def load_training_data(train_dir,num_train=1000,num_test=100):
-    X = np.zeros((num_train,101,1998))
-    X_test = np.zeros((num_test,101,1998))
+    X = np.zeros((num_train,freq_n,sample_n))
+    X_test = np.zeros((num_test,freq_n,sample_n))
 
     for i in range(num_train):
         X[i,:,:] = graph_spectrogram(train_dir + "/train" + str(i) + ".wav")
@@ -33,15 +34,12 @@ def load_training_data(train_dir,num_train=1000,num_test=100):
         X_test[i,:,:] = graph_spectrogram(train_dir + "/traintest" + str(i) + ".wav")
 
 
-    return X, X_test
+    return X.reshape(num_train,sample_n,freq_n), X_test.reshape(num_test,sample_n,freq_n)
 
 
-X, X_test = load_training_data(train_dir,200,50)
+X, X_test = load_training_data(train_dir)
 
-X = X.reshape(200,1998,101)
 
-Tx = 1998 # The number of time steps input to the model from the spectrogram
-n_freq = 101 # Number of frequencies input to the model at each time step of the spectrogram
 Ty = 1375 # The number of time steps in the output of the model
 
 def model(input_shape):
@@ -88,7 +86,7 @@ def model(input_shape):
 
 
 
-model = model(input_shape = (Tx, n_freq))
+model = model(input_shape = (sample_n, freq_n))
 model.summary()
 
 opt = Adam(lr=0.0001, beta_1=0.9, beta_2=0.999, decay=0.01)
@@ -96,21 +94,23 @@ model.compile(loss='binary_crossentropy', optimizer=opt, metrics=["accuracy"])
 
 model.fit(X, Y, batch_size = 5, epochs=10)
 
-loss, acc = model.evaluate(X_dev, Y_dev)
-print("Dev set accuracy = ", acc)
+model.save("audio_wake_model.h5")
 
-
-def detect_triggerword(filename):
-    plt.subplot(2, 1, 1)
-
-    x = graph_spectrogram(filename)
-    # the spectogram outputs (freqs, Tx) and we want (Tx, freqs) to input into the model
-    x  = x.swapaxes(0,1)
-    x = np.expand_dims(x, axis=0)
-    predictions = model.predict(x)
-
-    plt.subplot(2, 1, 2)
-    plt.plot(predictions[0,:,0])
-    plt.ylabel('probability')
-    plt.show()
-    return predictions
+# loss, acc = model.evaluate(X_dev, Y_dev)
+# print("Dev set accuracy = ", acc)
+#
+#
+# def detect_triggerword(filename):
+#     plt.subplot(2, 1, 1)
+#
+#     x = graph_spectrogram(filename)
+#     # the spectogram outputs (freqs, Tx) and we want (Tx, freqs) to input into the model
+#     x  = x.swapaxes(0,1)
+#     x = np.expand_dims(x, axis=0)
+#     predictions = model.predict(x)
+#
+#     plt.subplot(2, 1, 2)
+#     plt.plot(predictions[0,:,0])
+#     plt.ylabel('probability')
+#     plt.show()
+#     return predictions
